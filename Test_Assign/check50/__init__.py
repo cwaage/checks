dd4d3315a738bff787fafdff7b1d11288685d240
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-29 16:58:02 -0700

    Create __init__.py

---
+++ Test_Assign/check50/__init__.py
@@ -0,0 +1,18 @@
+from check50 import *
+
+class Test_Assign(Checks):
+
+	@check()
+	def exists(self):
+		"""test.c exists"""
+		self.require("test.c")
+
+	@check("exists")
+	def compiles(self):
+		"""test.c compiles"""
+		self.spawn("clang -o test test.c -lcs50 -lm").exit(0)
+
+	@check("compiles")
+	def test_41_cents(self):
+		"""Input of string Waage yields and output of Waage\n"""
+		self.spawn("./test").stdin("Waage").stdout("Waage\n").exit(0)
