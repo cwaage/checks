d2b032678148e53a61a68c78c5184a0f1e839bbc
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-30 05:18:43 -0700

    Update __init__.py

--- glocal/check50/__init__.py
+++ glocal/check50/__init__.py
@@ -1,6 +1,6 @@
 from check50 import *
 
-class Test_Assign(Checks):
+class glocal(Checks):
 
 	@check()
 	def exists(self):
@@ -15,19 +15,19 @@ class Test_Assign(Checks):
 	@check("compiles")
 	def test_in_five(self):
 		"""Input of 5 yields and output of 10\n"""
-		self.spawn("./test").stdin("5").stdout("10\n").exit(0)
+		self.spawn("./glocal").stdin("5").stdout("10\n").exit(0)
 	
 	@check("compiles")
 	def test_in_neg_five(self):
 		"""Input of 10 yields and output of 15\n"""
-		self.spawn("./test").stdin("10").stdout("15\n").exit(0)
+		self.spawn("./glocal").stdin("10").stdout("15\n").exit(0)
 	
 	@check("compiles")
     	def test_reject_negative(self):
         	"""rejects a negative input like -.1"""
-        	self.spawn("./text").stdin("-1").reject()
+        	self.spawn("./glocal").stdin("-1").reject()
 		
 	@check("compiles")
     	def test_reject_foo(self):
         	"""rejects a non-numeric input of "foo" """
-        	self.spawn("./cash").stdin("foo").reject()
+        	self.spawn("./glocal").stdin("foo").reject()
