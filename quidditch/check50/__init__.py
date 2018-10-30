65e902d0ad4b23ba2d17599fa23e7f64b81137de
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-30 09:52:02 -0700

    Update __init__.py

--- quidditch/check50/__init__.py
+++ quidditch/check50/__init__.py
@@ -12,12 +12,8 @@ class quidditch(Checks):
 		"""quidditch.c compiles"""
 		self.spawn("clang -o quidditch quidditch.c -lcs50 -lm").exit(0)
 
-	@check("compiles")
-	def test_in_five(self):
-		"""Input of 5 yields and output of 10\n"""
-		self.spawn("./quidditch").stdin("5").stdout("10\n").exit(0)
+	#@check("compiles")
+	#def test_2_true(self):
+		#"""Input of 5 yields and output of 10\n"""
+		#self.spawn("./quidditch").stdin("2",1).stdout("Your team's final score is: 170\n").exit(0)
 	
-	@check("compiles")
-	def test_in_neg_five(self):
-		"""Input of 10 yields and output of 15\n"""
-		self.spawn("./quidditch").stdin("10").stdout("15\n").exit(0)
