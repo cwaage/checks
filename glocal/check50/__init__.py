7107dc5223a668148fba9586b9a53d8c85e10721
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-30 05:05:37 -0700

    Update __init__.py

--- glocal/check50/__init__.py
+++ glocal/check50/__init__.py
@@ -13,6 +13,11 @@ class Test_Assign(Checks):
 		self.spawn("clang -o test glocal.c -lcs50 -lm").exit(0)
 
 	@check("compiles")
-	def test_41_cents(self):
-		"""Input of string Waage yields and output of Waage\n"""
-		self.spawn("./test").stdin("Waage").stdout("Waage\n").exit(0)
+	def test_in_five(self):
+		"""Input of 5 yields and output of 10\n"""
+		self.spawn("./test").stdin("5").stdout("10\n").exit(0)
+	
+	@check("compiles")
+	def test_in_neg_five(self):
+		"""Input of -5 yields and output of 0\n"""
+		self.spawn("./test").stdin("-5").stdout("0\n").exit(0)
