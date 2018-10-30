f9055eb3c4d4467d6f2abeffdc3761bc45c37a40
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-30 05:52:09 -0700

    Added test reject in init.py

--- glocal/check50/__init__.py
+++ glocal/check50/__init__.py
@@ -22,12 +22,12 @@ class glocal(Checks):
 		"""Input of 10 yields and output of 15\n"""
 		self.spawn("./glocal").stdin("10").stdout("15\n").exit(0)
 	
-	# @check("compiles")
-    	# def test_reject_negative(self):
-        	# """rejects a negative input like -.1"""
-        	# self.spawn("./glocal").stdin("-1").reject()
+	@check("compiles")
+    	def test_reject_negative(self):
+        	"""rejects a negative input like -.1"""
+        	self.spawn("./glocal").stdin("-1").reject()
 		
-	# @check("compiles")
-    	# def test_reject_foo(self):
-        	# """rejects a non-numeric input of "foo" """
-        	# self.spawn("./glocal").stdin("foo").reject()
+	@check("compiles")
+    	def test_reject_foo(self):
+        	"""rejects a non-numeric input of "foo" """
+        	self.spawn("./glocal").stdin("foo").reject()
