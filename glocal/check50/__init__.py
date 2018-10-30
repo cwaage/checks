bd3b221191f3b796a80ea6e62f0a8a144a61a779
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-30 05:50:34 -0700

    Update __init__.py

--- glocal/check50/__init__.py
+++ glocal/check50/__init__.py
@@ -17,10 +17,10 @@ class glocal(Checks):
 		"""Input of 5 yields and output of 10\n"""
 		self.spawn("./glocal").stdin("5").stdout("10\n").exit(0)
 	
-	# @check("compiles")
-	# def test_in_neg_five(self):
-		# """Input of 10 yields and output of 15\n"""
-		# self.spawn("./glocal").stdin("10").stdout("15\n").exit(0)
+	@check("compiles")
+	def test_in_neg_five(self):
+		"""Input of 10 yields and output of 15\n"""
+		self.spawn("./glocal").stdin("10").stdout("15\n").exit(0)
 	
 	# @check("compiles")
     	# def test_reject_negative(self):
