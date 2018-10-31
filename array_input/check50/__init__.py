f16531f47a51da242fff1d959724a58b2e383648
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 06:00:50 -0700

    Update __init__.py

--- array_input/check50/__init__.py
+++ array_input/check50/__init__.py
@@ -12,17 +12,8 @@ class Array_Input(Checks):
         """array_input.c compiles"""
         self.spawn("clang -o array_input array_input.c -lcs50 -lm").exit(0)
 
-    @check("compiles")
-    def test_c_100(self):
-        """Convert from Celcius True Temp 100 Yields 212.0\n"""
-        self.spawn("./array_input").stdin("1").stdin("100").stdout("212.0\n").exit(0)
+    # @check("compiles")
+    # def test_c_100(self):
+        # """Convert from Celcius True Temp 100 Yields 212.0\n"""
+        # self.spawn("./array_input").stdin("1").stdin("100").stdout("212.0\n").exit(0)
 
-    @check("compiles")
-    def test_f_212(self):
-        """Convert from Celcius False Temp 212 Yields 100.0\n"""
-        self.spawn("./array_input").stdin("0").stdin("212").stdout("100.0\n").exit(0)
-
-    @check("compiles")
-    def test_c_25(self):
-            """Convert from Celcius True Temp 25 Yields 77.0\n"""
-            self.spawn("./array_input").stdin("1").stdin("25").stdout("77.0\n").exit(0)
