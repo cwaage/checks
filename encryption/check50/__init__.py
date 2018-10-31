1845c40ad7387587d9d88bfce16a2d5ff4586cf4
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 10:35:53 -0700

    Update __init__.py

--- encryption/check50/__init__.py
+++ encryption/check50/__init__.py
@@ -11,3 +11,8 @@ class Encryption(Checks):
 	def compiles(self):
 		"""encryption.c compiles"""
 		self.spawn("clang -o encryption encryption.c -lcs50 -lm").exit(0)
+		
+	@check("compiles")
+	def test_175_114(self):
+		"""Test 175 and 114 Yields Success\n"""
+		self.spawn("./encryption").stdin("175").stdin("114").stdout("Success\n").exit(0)
