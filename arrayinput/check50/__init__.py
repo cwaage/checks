fc6c69408285149ef0b35192d89c44e26b610bbb
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 08:05:58 -0700

    Update __init__.py

--- arrayinput/check50/__init__.py
+++ arrayinput/check50/__init__.py
@@ -21,10 +21,3 @@ class Array_Input(Checks):
     def test_reject_paris(self):
 	    """rejects a non-numeric input of "Paris" """
 	    self.spawn("./array_input").stdin("Paris").reject()
-		
-	@check("compiles")
-	def test_c_100(self):
-		"""Convert from Celcius True Temp 100 Yields 212.0\n"""
-		self.spawn("./array_input").stdin("1").stdin("100").stdout("212.0\n").exit(0)
-	
-	
