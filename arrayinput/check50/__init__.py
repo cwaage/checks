5f0d686d25f430cccfe070fb5ef236f0bb2077c4
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 07:44:59 -0700

    Update __init__.py

--- arrayinput/check50/__init__.py
+++ arrayinput/check50/__init__.py
@@ -3,29 +3,29 @@ from check50 import *
 class Array_Input(Checks):
 	
 	@check()
-    	def exists(self):
-        	"""array_input.c exists"""
-        	self.require("array_input.c")
-	
-    	@check("exists")
-    	def compiles(self):
-        	"""array_input.c compiles"""
-        	self.spawn("clang -o array_input array_input.c -lcs50 -lm").exit(0)
+	def exists(self):
+ 		"""array_input.c exists"""
+  		self.require("array_input.c")
+
+	@check("exists")
+  	def compiles(self):
+  		"""array_input.c compiles"""
+  		self.spawn("clang -o array_input array_input.c -lcs50 -lm").exit(0)
         
-    	@check("compiles")
-    	def test_reject_zero(self):
-	    	"""rejects input 0"""
-	    	self.spawn("./array_input").stdin("0").reject()
+	@check("compiles")
+	def test_reject_zero(self):
+		"""rejects input 0"""
+		self.spawn("./array_input").stdin("0").reject()
         
-    	@check("compiles")
-    	def test_reject_eleven(self):
-        	"""rejects input 11"""
-        	self.spawn("./array_input").stdin("11").reject()
+	@check("compiles")
+	def test_reject_eleven(self):
+		"""rejects input 11"""
+		self.spawn("./array_input").stdin("11").reject()
 	
-    	@check("compiles")
-    	def test_reject_levi(self):
+	@check("compiles")
+	def test_reject_paris(self):
 		"""rejects a non-numeric input of "Paris" """
-	   	self.spawn("./array_input").stdin("Paris").reject()
+		self.spawn("./array_input").stdin("Paris").reject()
 	# @check("compiles")
     	# def test_c_100(self):
        		# """Convert from Celcius True Temp 100 Yields 212.0\n"""
