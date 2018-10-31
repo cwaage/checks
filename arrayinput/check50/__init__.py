4880eb98a789e3d12df4ee18492cbf14e08fdb57
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 07:52:03 -0700

    Update __init__.py

--- arrayinput/check50/__init__.py
+++ arrayinput/check50/__init__.py
@@ -2,12 +2,12 @@ from check50 import *
 
 class Array_Input(Checks):
 
-	@check()
-	def exists(self):
-		"""array_input.c exists"""
-		self.require("array_input.c")
+    @check()
+    def exists(self):
+        """array_input.c exists"""
+        self.require("array_input.c")
 
-	@check("exists")
-	def compiles(self):
-		"""array_input.c compiles"""
- 		self.spawn("clang -o array_input array_input.c -lcs50 -lm").exit(0)
+    @check("exists")
+    def compiles(self):
+        """array_input.c compiles"""
+        self.spawn("clang -o array_input array_input.c -lcs50 -lm").exit(0)
