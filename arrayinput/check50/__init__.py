e6863fc66a84960870ee60ead766170eaa087d28
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 08:03:16 -0700

    Update __init__.py

--- arrayinput/check50/__init__.py
+++ arrayinput/check50/__init__.py
@@ -17,12 +17,6 @@ class Array_Input(Checks):
         """rejects a input 0"""
         self.spawn("./array_input").stdin("0").reject()
 	
-	@check("compiles")
-	def test_in_four(self):
-		"""Input of 4 yields and {5,6,7,8} output of 5 6 7 8 """
-		self.spawn("./array_input").stdin("4").stdin("5").stdin("6").stdin("7").stdin("8").stdout("5 6 7 8 ").exit(0)
-		
-
     @check("compiles")
     def test_reject_paris(self):
 	    """rejects a non-numeric input of "Paris" """
