b004322835e313414b155d37bef32db5ac1bf4cd
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 10:17:45 -0700

    Create __init__.py

---
+++ encryption/check50/__init__.py
@@ -0,0 +1,13 @@
+from check50 import *
+
+class Encryption(Checks):
+
+	@check()
+	def exists(self):
+		"""encryption.c exists"""
+		self.require("encryption.c")
+
+	@check("exists")
+	def compiles(self):
+		"""encryption.c compiles"""
+		self.spawn("clang -o encryption encryption.c -lcs50 -lm").exit(0)
