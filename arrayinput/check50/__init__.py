bb2118ae5aa25fc55faa1c1bccaacbebfc3ad6ee
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-10-31 07:55:11 -0700

    Update __init__.py

--- arrayinput/check50/__init__.py
+++ arrayinput/check50/__init__.py
@@ -11,3 +11,14 @@ class Array_Input(Checks):
     def compiles(self):
         """array_input.c compiles"""
         self.spawn("clang -o array_input array_input.c -lcs50 -lm").exit(0)
+        
+    @check("compiles")
+    def test_reject_zero(self):
+        """rejects a input 0"""
+        self.spawn("./array_input").stdin("0").reject()
+		
+
+    @check("compiles")
+    def test_reject_paris(self):
+	    """rejects a non-numeric input of "Pari" """
+	    self.spawn("./array_input").stdin("Paris").reject()
