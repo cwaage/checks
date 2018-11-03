892512405c39fa16e7851a56cdf36fed2954053e
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-03 10:12:41 -0700

    Update __init__.py

--- fibonacci/check50/__init__.py
+++ fibonacci/check50/__init__.py
@@ -14,6 +14,6 @@ class Fib25(Checks):
  
     @check("compiles")
     def test_pass_25_values(self):
-      """Check Output of the First 25 Fibonacci Values"""
-      """0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 """
+      """Check Output of the First 25 Fibonacci Values!
+      0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 """
       self.spawn("./fib_25").stdout("0 1 1 2 3 5 8 13 21 34 55 89 144 233 377 610 987 1597 2584 4181 6765 10946 17711 28657 46368 ").exit(0)
