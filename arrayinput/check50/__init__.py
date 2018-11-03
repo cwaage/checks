2fe8410e7455d42d174e2b78b7e18065101258f5
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-03 16:56:29 -0700

    Update __init__.py

--- arrayinput/check50/__init__.py
+++ arrayinput/check50/__init__.py
@@ -26,10 +26,18 @@ class Array_Input(Checks):
     def test_reject_zoe(self):
 	    """rejects a non-numeric input of "Zoe" """
 	    self.spawn("./array_input").stdin("Zoe").reject()
+
+    @check("compiles")
+    def test_pass_3(self):
+	    """Accepts Array Size 3 && values 1,2,3 Output 1 2 3 """
+	    self.spawn("./array_input").stdin("3").stdin("1").stdin("2").stdin("3").stdout("1 2 3 ").exit(0)
 		
     @check("compiles")
     def test_pass_5(self):
 	    """Accepts Array Size 5 && values 1,2,3,4,5 Output 1 2 3 4 5 """
 	    self.spawn("./array_input").stdin("5").stdin("1").stdin("2").stdin("3").stdin("4").stdin("5").stdout("1 2 3 4 5 ").exit(0)
 		
-
+    @check("compiles")
+    def test_pass_8(self):
+	    """Accepts Array Size 8 && values 2,4,6,8,10,12,14,19 Output 2 4 6 8 10 12 14 19 """
+	    self.spawn("./array_input").stdin("8").stdin("2").stdin("4").stdin("6").stdin("8").stdin("10").stdin("12").stdin("14").stdin("19")..stdout("2 4 6 8 10 12 14 19 ").exit(0)
