15e3e6e8c88da98bbdfc1a95f4cf5fcede34cd87
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-03 17:20:22 -0700

    Update __init__.py

--- mean/check50/__init__.py
+++ mean/check50/__init__.py
@@ -26,3 +26,8 @@ class Mean(Checks):
     def test_pass_5(self):
 	    """Accepts Average with values values 5,4,9,27,3 Output 9.60 """
 	    self.spawn("./mean").stdin("5").stdin("4").stdin("9").stdin("27").stdin("3").stdout("9.60\n").exit(0)
+
+    @check("compiles")
+    def test_pass_5_more(self):
+	    """Accepts Average with values values 3,200,99,12,88 Output 80.40 """
+	    self.spawn("./mean").stdin("3").stdin("200").stdin("99").stdin("12").stdin("88").stdout("88.40\n").exit(0)
