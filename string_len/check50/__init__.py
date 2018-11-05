6eeb93b72556cfe14f129e6eb0f61558f1dbbb52
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-05 15:50:11 -0800

    Update __init__.py

--- string_len/check50/__init__.py
+++ string_len/check50/__init__.py
@@ -13,11 +13,11 @@ class String_Len(Checks):
         self.spawn("clang -o string_len string_len.c -lcs50 -lm").exit(0)
         
     @check("compiles")
-    def test_reject_Maia(self):
-      """rejects a non-numeric input of "Maia" """
-      self.spawn("./string_len").stdin("Maia").reject()
+    def test_reject_27(self):
+      """rejects a numeric input of 27 """
+      self.spawn("./string_len").stdin(27).reject()
 		
     @check("compiles")
-    def test_reject_Ewan(self):
-      """rejects a non-numeric input of "Ewan" """
-      self.spawn("./string_len").stdin("Ewan").reject()
+    def test_reject_4.0(self):
+      """rejects a numeric input of 4.0 """
+      self.spawn("./string_len").stdin(4.0).reject()
