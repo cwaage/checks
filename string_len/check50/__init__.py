49138c1369bc4765820d2c3c9e02cca6de411c7f
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-05 16:16:44 -0800

    Update __init__.py

--- string_len/check50/__init__.py
+++ string_len/check50/__init__.py
@@ -13,6 +13,11 @@ class String_Len(Checks):
         self.spawn("clang -o string_len string_len.c -lcs50 -lm").exit(0)
  
     @check("compiles")
+    def test_pass_0(self):
+        """Accepts String ""  Output 0\n """
+        self.spawn("./string_len").stdin("").stdout("0\n").exit(0)
+
+    @check("compiles")
     def test_pass_AK(self):
         """Accepts String "Amelia"  Output 6\n """
         self.spawn("./string_len").stdin("Amelia").stdout("6\n").exit(0)
