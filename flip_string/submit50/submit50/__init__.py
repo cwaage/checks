57fd835aab6fdf5c1c0fa1e7ac37c36988b4f81f
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-05 17:39:04 -0800

    Create __init__.py

---
+++ flip_string/submit50/submit50/__init__.py
@@ -0,0 +1,28 @@
+from check50 import *
+
+class Flip_String(Checks):
+
+    @check()
+    def exists(self):
+        """flip_string.c exists"""
+        self.require("flip_string.c")
+        
+    @check("exists")
+    def compiles(self):
+        """flip_string.c compiles"""
+        self.spawn("clang -o flip_string flip_string.c -lcs50 -lm").exit(0)
+        
+    @check("compiles")
+    def test_pass_amelia(self):
+        """Accepts String "amelia"  Yields "AmElIa" """
+        self.spawn("./flip_string").stdin("amelia").stdout("AmElIa").exit(0)
+        
+    @check("compiles")
+    def test_pass_CSP(self):
+        """Accepts String "sKYLAR"  Yields "SkYlAr" """
+        self.spawn("./flip_string").stdin("sKYLAR").stdout("SkYlAr").exit(0)
+        
+    @check("compiles")
+    def test_pass_CSP(self):
+        """Accepts String "KATaya"  Yields "kAtAyA" """
+        self.spawn("./flip_string").stdin("KATaya").stdout("kAtAyA").exit(0)
