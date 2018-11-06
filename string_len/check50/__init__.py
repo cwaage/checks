51b0eef263afc198022f1f1488ff7f3655068753
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-05 16:01:15 -0800

    Update __init__.py

--- string_len/check50/__init__.py
+++ string_len/check50/__init__.py
@@ -11,8 +11,8 @@ class String_Len(Checks):
     def compiles(self):
         """string_len.c compiles"""
         self.spawn("clang -o string_len string_len.c -lcs50 -lm").exit(0)
-        
+ 
     @check("compiles")
-    def test_reject_27(self):
-      """rejects a numeric input of 27 """
-      self.spawn("./string_len").stdin("27").reject()
+    def test_pass_3(self):
+        """Accepts String "Amelia Output 6\n """
+        self.spawn("./array_input").stdin("Amelia").stdout("6\n").exit(0)
