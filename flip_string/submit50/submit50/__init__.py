05bf6a0b2e6ffd7cd4a2bcda0d3dd5d6fe636aea
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-05 17:42:20 -0800

    Update __init__.py

--- flip_string/submit50/submit50/__init__.py
+++ flip_string/submit50/submit50/__init__.py
@@ -12,17 +12,17 @@ class Flip_String(Checks):
         """flip_string.c compiles"""
         self.spawn("clang -o flip_string flip_string.c -lcs50 -lm").exit(0)
         
-    @check("compiles")
-    def test_pass_amelia(self):
-        """Accepts String "amelia"  Yields "AmElIa" """
-        self.spawn("./flip_string").stdin("amelia").stdout("AmElIa").exit(0)
+    # @check("compiles")
+    # def test_pass_amelia(self):
+        # """Accepts String "amelia"  Yields "AmElIa" """
+        # self.spawn("./flip_string").stdin("amelia").stdout("AmElIa").exit(0)
         
-    @check("compiles")
-    def test_pass_Skylar(self):
-        """Accepts String "sKYLAR"  Yields "SkYlAr" """
-        self.spawn("./flip_string").stdin("sKYLAR").stdout("SkYlAr").exit(0)
+    # @check("compiles")
+    # def test_pass_Skylar(self):
+        # """Accepts String "sKYLAR"  Yields "SkYlAr" """
+        # self.spawn("./flip_string").stdin("sKYLAR").stdout("SkYlAr").exit(0)
         
-    @check("compiles")
-    def test_pass_Kat(self):
-        """Accepts String "KATaya"  Yields "kAtAyA" """
-        self.spawn("./flip_string").stdin("KATaya").stdout("kAtAyA").exit(0)
+    # @check("compiles")
+    # def test_pass_Kat(self):
+        # """Accepts String "KATaya"  Yields "kAtAyA" """
+        # self.spawn("./flip_string").stdin("KATaya").stdout("kAtAyA").exit(0)
