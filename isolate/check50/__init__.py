3c33f97ce6db3ea833d1044d8d1e6c2a5e48f6b4
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-13 15:37:08 -0800

    Update __init__.py

--- isolate/check50/__init__.py
+++ isolate/check50/__init__.py
@@ -11,3 +11,8 @@ class Isolate(Checks):
     def compiles(self):
         """isolate.c compiles"""
         self.spawn("clang -o isolate isolate.c -lcs50 -lm").exit(0)
+        
+    @check("compiles")
+    def test_pass_amelia(self):
+        """Accepts Strings "dog cat"  Yields "d o g c a t" """
+        self.spawn("./isolate dog cat").stdout("d\n").stdout("o\n").stdout("g\n").stdout("c\n").stdout("a\n").stdout("t\n").exit(0)
