7cbdb19d022348ae10ca8a18ebbc143cc39bcb56
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-13 16:46:38 -0800

    Update __init__.py

--- isolate/check50/__init__.py
+++ isolate/check50/__init__.py
@@ -14,15 +14,15 @@ class Isolate(Checks):
         
     @check("compiles")
     def test_fail_no_args(self):
-        """Accepts Strings "Correctly Hands Too Few Arguments"""
+        """Accepts Strings Correctly Hands Too Few Arguments"""
         self.spawn("./isolate").stdout("").exit(1)  
 
     @check("compiles")
     def test_fail_two_many_args(self):
-        """Accepts Strings "Correctly Hands Too Many Arguments"""
+        """Accepts Strings Correctly Hands Too Many Arguments"""
         self.spawn("./isolate Luke Thian Skylar").stdout("").exit(1) 
 
     @check("compiles")
     def test_pass_dog_cat(self):
-        """Accepts Strings "dog cat"  Yields "D\\n o\\n g\\n C\\n a\\n t\\n" """
+        """Accepts Strings "Dog Cat"  Yields "D\\n o\\n g\\n C\\n a\\n t\\n" """
         self.spawn("./isolate Dog Cat").stdout("D\n").stdout("o\n").stdout("g\n").stdout("C\n").stdout("a\n").stdout("t\n").exit(0)
