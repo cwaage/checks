2444c4c9db27667263a138db34a760ae005e3ec4
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-25 16:53:48 -0800

    Update __init__.py

--- geometry/check50/__init__.py
+++ geometry/check50/__init__.py
@@ -13,19 +13,29 @@ class Geometry(Checks):
         """geometry.c compiles"""
         self.spawn("clang -std=c11 -o geometry geometry.c -lcs50 -lm").exit(0)
 
+    @check("exists")
+    def test_fail_no_args(self):
+        """Correctly handles no arguments"""
+        self.spawn("./geometry").exit(1)
+        
+    @check("exists")
+    def test_fail_too_manyh_args(self):
+        """Correctly handles too many arguments"""
+        self.spawn("./geometry area perimeter").exit(1)        
+
     @check("compiles")
     def test1(self):
-        """Compiles and then runs invalid menu item 1, then area of rectangle 10 x 10"""
+        """Compiles checks invalid options for shape code, then area of rectangle 10 x 10"""
         self.spawn("./geometry area").stdin("-1").stdin("0").stdin("1").stdin("10").stdin("10").stdout("The rectangle's area is 100.\n").exit(0)
 
     @check("compiles")
     def test2(self):
-        """Compiles and then runs invalid rectangle lengths (negative and 0), then area of rectangle 4 x 5"""
+        """Compiles and then checks invalid rectangle lengths (negative and 0), then area of rectangle 4 x 5"""
         self.spawn("./geometry area").stdin("1").stdin("-1").stdin("0").stdin("4").stdin("5").stdout("The rectangle's area is 20.\n").exit(0)
 
     @check("compiles")
     def test3(self):
-        """Compiles and then runs invalid menu item 2, then area of square 4"""
+        """Compiles and then runs invalid shape code, then area of square 4"""
         self.spawn("./geometry area").stdin("-1").stdin("4").stdin("2").stdin("4").stdout("The square's area is 16.\n").exit(0)
 
     @check("compiles")
