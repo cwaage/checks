8313427e6702f8187854e5e98f34ea240728bc36
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-11-25 16:46:39 -0800

    Update __init__.py

--- geometry/check50/__init__.py
+++ geometry/check50/__init__.py
@@ -16,22 +16,22 @@ class Geometry(Checks):
     @check("compiles")
     def test1(self):
         """Compiles and then runs invalid menu item 1, then area of rectangle 10 x 10"""
-        self.spawn("./geometry area").stdin("-1").stdin("0").stdin("1").stdin("10").stdin("10").stdout("The rectangle's area is 100\n").exit(0)
+        self.spawn("./geometry area").stdin("-1").stdin("0").stdin("1").stdin("10").stdin("10").stdout("The rectangle's area is 100.\n").exit(0)
 
     @check("compiles")
     def test2(self):
         """Compiles and then runs invalid rectangle lengths (negative and 0), then area of rectangle 4 x 5"""
-        self.spawn("./geometry area").stdin("1").stdin("-1").stdin("0").stdin("4").stdin("5").stdout("The rectangle's area is 20\n").exit(0)
+        self.spawn("./geometry area").stdin("1").stdin("-1").stdin("0").stdin("4").stdin("5").stdout("The rectangle's area is 20.\n").exit(0)
 
     @check("compiles")
     def test3(self):
         """Compiles and then runs invalid menu item 2, then area of square 4"""
-        self.spawn("./geometry").stdin("1").stdin("-1").stdin("4").stdin("2").stdin("4").stdout("The square's area is 16.\n").exit(0)
+        self.spawn("./geometry area").stdin("-1").stdin("4").stdin("2").stdin("4").stdout("The square's area is 16.\n").exit(0)
 
     @check("compiles")
     def test4(self):
         """Compiles and then runs invalid square length negative or 0, then area of square 7"""
-        self.spawn("./geometry").stdin("1").stdin("2").stdin("-7").stdin("0").stdin("7").stdout("The square's area is 49.\n").exit(0)
+        self.spawn("./geometry area").stdin("2").stdin("-7").stdin("0").stdin("7").stdout("The square's area is 49.\n").exit(0)
 
     @check("compiles")
     def test5(self):
