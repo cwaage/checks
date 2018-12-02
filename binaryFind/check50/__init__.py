12e1ef92f09680d8743ed7bc5d1d63e345ed3484
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-12-02 15:00:34 -0800

    Create __init__.py

---
+++ binaryFind/check50/__init__.py
@@ -0,0 +1,18 @@
+from check50 import *
+
+class Binary_Find(Checks):
+
+    @check()
+    def exists(self):
+        """binaryFind.c exists"""
+        self.require("binaryFind.c")
+        
+    @check("exists")
+    def compiles(self):
+        """binaryFind.c compiles"""
+        self.spawn("clang -o binaryFind binaryFind.c -lcs50 -lm").exit(0)
+        
+    @check("compiles")
+    def test_found_40(self):
+        """Finds 40 at index = 6, count = 4"""
+        self.spawn("./flip_string").stdin("40").stdout("6 4").exit(0)
