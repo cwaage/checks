61bdf2850372ed9c9a9d2598b29446d72bfd6075
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-12-06 19:40:51 -0800

    Create __init__.py

---
+++ weave/check50/__init__.py
@@ -0,0 +1,29 @@
+from check50 import *
+
+
+class Weave(Checks):
+
+    @check()
+    def exists(self):
+        """weave.c exists"""
+        self.require("weave.c")
+
+    @check("exists")
+    def compiles(self):
+        """weave.c compiles"""
+        self.spawn("clang -std=c11 -o weave weave.c -lcs50 -lm").exit(0)
+
+    @check("compiles")
+    def test_fail_no_args(self):
+        """Correctly handles no arguments"""
+        self.spawn("./weave").stdout("").exit(1)
+        
+    @check("compiles")
+    def test_fail_too_manyh_args(self):
+        """Correctly handles too many arguments"""
+        self.spawn("./weave bob cat jones").stdout("").exit(1)  
+    
+    @check("compiles")
+    def test_pass_cat_dog_inputs(self):
+        """Correctly handles \".\/computeAve cat dog \" """
+        self.spawn("./computeAve cat dog").stdout("cot\n").exit(0) 
