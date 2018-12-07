2c336bd3d0c8151fce5fe1f512e0e0bc358924a6
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-12-06 19:31:23 -0800

    Update __init__.py

--- computeAve/check50/__init__.py
+++ computeAve/check50/__init__.py
@@ -32,8 +32,13 @@ class Computer_Average(Checks):
     def test_pass_1_inputs(self):
         """Correctly handles a single deposit"""
         self.spawn("./computeAve 1").stdin("50.55").stdout("Your average deposit is \$50.55\n").exit(0)   
-  
+    
+    @check("compiles")
+    def test_pass_3_inputs(self):
+        """Correctly handles 3 deposits"""
+        self.spawn("./computeAve 3").stdin("30.30").stdin("2000.79").stdin("0.05").stdout("Your average deposit is \$677.05\n").exit(0)
+        
     @check("compiles")
     def test_pass_5_inputs(self):
-        """Correctly handles 5 deposits arguments"""
+        """Correctly handles 5 deposits"""
         self.spawn("./computeAve 5").stdin("5.99").stdin("4.50").stdin("200.02").stdin("100.50").stdin("20.02").stdout("Your average deposit is \$66.21\n").exit(0)   
