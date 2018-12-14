da49c3f3cb6e255821479ac0a6d48ba7dfebef75
Chris Waage <40725414+cwaage@users.noreply.github.com>
2018-12-14 08:11:10 -0800

    Update __init__.py

--- simpleInsert/check50/__init__.py
+++ simpleInsert/check50/__init__.py
@@ -14,13 +14,13 @@ class Simple_Insert(Checks):
 
     @check("compiles")
     def test_pass_same_low(self):
-        """Adds 0.0 to Array  Yields: -7.70 -3.50 5.60 7.00 8.25 9.67 18.30 """
-        self.spawn("./simpleInsert").stdin("-3.5").stdout("-3.50 -3.5 5.60 7.00 8.25 9.67 18.30").exit(0)
+        """Adds 0.0 to Array  Yields: -3.50 -3.50 5.60 7.00 8.25 9.67 18.30 """
+        self.spawn("./simpleInsert").stdin("-3.5").stdout("-3.50 -3.50 5.60 7.00 8.25 9.67 18.30").exit(0)
         
     @check("compiles")
     def test_pass_zero(self):
-        """Adds 0.0 to Array  Yields: -7.70 -3.50 5.60 7.00 8.25 9.67 18.30 """
-        self.spawn("./simpleInsert").stdin("0.0").stdout("-3.50 0.0 5.60 7.00 8.25 9.67 18.30").exit(0)
+        """Adds 0.0 to Array  Yields: -3.50 0.00 5.60 7.00 8.25 9.67 18.30 """
+        self.spawn("./simpleInsert").stdin("0.0").stdout("-3.50 0.00 5.60 7.00 8.25 9.67 18.30").exit(0)
     
     @check("compiles")
     def test_pass_low(self):
